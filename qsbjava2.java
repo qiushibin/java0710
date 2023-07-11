15e9e2f31ca67ecca8aee99f39d4a1744192b23a
qiushibin <qiu.shibin@cxtc.com>
2023-07-11 11:28:16 +0800

    new file:   .vscode/launch.json 	modified:   qsbjava2.java

--- qsbjava2.java
+++ qsbjava2.java
@@ -1,8 +1,18 @@
+
+import java.util.Scanner;
+
 public class qsbjava2{
     int ok = (int) (Math.random() *10);
 
-    public void pp(){
-         
+    public void pp() {
+        
         System.out.println(ok);
+        System.out.print("waiting in ... ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            
+            String a = scanner.nextLine();
+            System.out.println("input:" +a);
+        }
+    
     }
 }

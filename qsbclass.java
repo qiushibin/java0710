5d3ed89736418f739f5500812d0d90b9b93df19d
qiushibin <qiu.shibin@cxtc.com>
2023-07-18 14:29:29 +0800

    new file:   qsbclass.java

---
+++ qsbclass.java
@@ -0,0 +1,17 @@
+
+public class qsbclass {
+    
+    double   tmpf = 1.1;
+
+    public void pf(int  status){
+        if (status == 1)
+            System.out.println("qsbclass 1");
+        else
+            System.out.println("qsbclass 0");
+    } 
+
+    void pf2(){
+        System.out.println("pf 2");
+    }
+}
+  
